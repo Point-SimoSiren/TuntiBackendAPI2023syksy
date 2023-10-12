b8890f5ebd5e5fc69c1e6d0bd6f9f9331b66eb7f
Simo Sirén <simo.siren1@careeria.fi>
2023-10-12 09:12:10 +0300

    valmis

--- TuntiBackendAPI2023s/Models/tuntidbContext.cs
+++ TuntiBackendAPI2023s/Models/tuntidbContext.cs
@@ -26,7 +26,7 @@ namespace TuntiBackendAPI2023s.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=duuniserver.database.windows.net,1433;Database=tuntidb;User ID=sirensimo;Password=KysyOpelta;");
+                optionsBuilder.UseSqlServer("Server=duuniserver.database.windows.net,1433;Database=tuntidb;User ID=sirensimo;Password=Fullstack7777!;");
             }
         }
 
